00d2121db35a4571660e6f1e016aa779d9330420
Glenn <glennake.1@gmail.com>
2021-01-19 22:48:28 +0000

    update webex API URL

--- sample.py
+++ sample.py
@@ -161,7 +161,7 @@ def create_message_with_attachment(rid, msgtxt, attachment):
         "authorization": "Bearer " + teams_token,
     }
 
-    url = "https://api.ciscospark.com/v1/messages"
+    url = "https://webexapis.com/v1/messages"
     data = {"roomId": rid, "attachments": [attachment], "markdown": msgtxt}
     response = requests.post(url, json=data, headers=headers)
     return response.json()
@@ -175,7 +175,7 @@ def get_attachment_actions(attachmentid):
         "authorization": "Bearer " + teams_token,
     }
 
-    url = "https://api.ciscospark.com/v1/attachment/actions/" + attachmentid
+    url = "https://webexapis.com/v1/attachment/actions/" + attachmentid
     response = requests.get(url, headers=headers)
     return response.json()
 
